481219d3ef9b4b486e5d2dad200ae2daba2ceecf
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-27 11:05:58 +0100

    wip

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[])
 {
 
-	const char * _i = getOption("-i",(char*)0, argc, argv);
+    const char * _i = getOption("-i",(char*)0, argc, argv);
     const char * _o = getOption("-o","output.txt", argc, argv);
 	
 	return 0;
