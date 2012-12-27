1a93f6de6502b5cfd98c2e8e40013d84fbeee70a
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-27 11:18:11 +0100

    wip

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 
 int main(int argc, char const *argv[])
 {
+    const char *help_options = "\n\tDocumentation\n \toptions: \n\t-u optional:\n\t-o <output file> // by default output.txt \n\n";
+
+
+    if(argc < 2)
+    {
+        printf("%s", help_options);
+        return 1;
+    }                    
 
     const char * _i = getOption("-i",(char*)0, argc, argv);
     const char * _o = getOption("-o","output.txt", argc, argv);
-	
-	return 0;
+    
+    return 0;
 }
