ad3e34dfc8fb3ee96c1db39c05aa9ea1d4367c62
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-27 11:56:34 +0100

    wip

--- functions.h
+++ functions.h
@@ -8,6 +8,42 @@
 #define true 1
 #define false 0
 
+struct constants
+{
+	char* name;
+	char* typ;
+	struct constants *next;
+};
+
+struct variables
+{
+	char* name;
+	char* typ;
+	struct variables *next;
+};
+
+struct types
+{
+	struct variables* v;
+	struct types* next;
+};
+
+struct functions
+{
+	struct constants* c;
+	struct variables* v;
+	struct types* t;
+	struct functions* next;
+};
+
+struct procedures
+{
+	struct constants* c;
+	struct variables* v;
+	struct types* t;
+	struct procedures* next;
+};
+
 inline const char*  getOption(const char * name, const char * defaut,
 								int argc, char ** argv);
 char getFileData(char patern, char *buf, FILE *f);
